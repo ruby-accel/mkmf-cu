0172cb5322d333d248ecc2a8f43a819458112757
Takashi Tamura <tamuratak@users.noreply.github.com>
2016-02-23 16:41:51 +0900

    minor edit

--- test/test_mkmf-cu.rb
+++ test/test_mkmf-cu.rb
@@ -5,7 +5,7 @@ class TestMkmfCuOpt < Test::Unit::TestCase
   def test_opt
 
     opt_h = Hash.new{|h, k| h[k] = [] }
-    argv = ["--mkmf-cu-ext=cxx", "-I.", "-I/opt/local/include/ruby-2.3.0/x86_64-darwin13", "-I/opt/local/include/ruby-2.3.0/ruby/backward", "-I/opt/local/include/ruby-2.3.0", "-I../../../../ext/culib", "-I/opt/local/include", "-I/opt/local/include", "-D_XOPEN_SOURCE", "-D_DARWIN_C_SOURCE", "-D_DARWIN_UNLIMITED_SELECT", "-D_REENTRANT", "-fno-common", "-pipe", "-Os", "-stdlib=libc++", "-x", "cu", "-O2", "-Wall", "-I/Developer/NVIDIA/CUDA-7.5/samples/common/inc/", "-arch", "x86_64", "-o", "culib.o", "-c", "../../../../ext/culib/culib.cxx"]
+    argv = ["--mkmf-cu-ext=cxx", "-I.", "-I/opt/local/include", "-D_XOPEN_SOURCE", "-D_DARWIN_C_SOURCE", "-fno-common", "-pipe", "-Os", "-stdlib=libc++", "-x", "cu", "-O2", "-Wall", "-arch", "x86_64", "-o", "culib.o", "-c", "culib.cxx"]
     assert_equal(["-arch", "-shared", "-rdynamic", "-dynamic", "-bundle", "-stdlib", "-pipe"].sort,
                  parse_ill_short(argv, opt_h).sort)
 
@@ -14,7 +14,7 @@ class TestMkmfCuOpt < Test::Unit::TestCase
                  compiler_option(h))
 
     opt_h = Hash.new{|h, k| h[k] = [] }
-    argv = ["--mkmf-cu-ext=cxx", "-dynamic", "-bundle", "-o", "culib.bundle", "culib.o", "-L.", "-L/opt/local/lib", "-L/opt/local/lib", "-L.", "-L/opt/local/lib", "-Wl,-headerpad_max_install_names", "-fstack-protector", "-L/opt/local/lib", "-Wl,-undefined,dynamic_lookup", "-Wl,-multiply_defined,suppress", "-L/opt/local/lib", "-arch", "x86_64", "-lruby.2.3.0", "-lc++", "-lpthread", "-ldl", "-lobjc"]
+    argv = ["--mkmf-cu-ext=cxx", "-dynamic", "-bundle", "-o", "culib.bundle", "culib.o", "-L.", "-L/opt/local/lib", "-Wl,-headerpad_max_install_names", "-fstack-protector", "-L/opt/local/lib", "-Wl,-undefined,dynamic_lookup", "-Wl,-multiply_defined,suppress", "-arch", "x86_64", "-lruby.2.3.0", "-lc++", "-lpthread", "-ldl", "-lobjc"]
     parse_ill_short_with_arg(argv, opt_h)
     assert_equal(["-headerpad_max_install_names",
                   "-undefined,dynamic_lookup",
